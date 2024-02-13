0d0b38121084752affd85f35e2eb1d3752a63a04
oleg_vl <@nitro-pc>
2024-02-13 10:38:15 +0300

    Client.py
    добавлены базовые методы классов каждой таблицы для получения данных из апи. Обработка в классе Locations добавить в другие классы по аналогии для совместимости с методом класса Client.df_to_sql

--- source/client.py
+++ source/client.py
@@ -1,8 +1,13 @@
+import logging
+
 import requests
 import pandas as pd
 
+from sqlalchemy.types import Integer, String
+
 from database.schema import engine
 
+logger = logging.getLogger('client')
 
 class Client():
     def __init__(self) -> None:
@@ -22,17 +27,13 @@ class Client():
             for i in range(1, int(pages) + 1)
         ]
 
+        logger.info('Data retrieved: {} col, {} pages'.format(self.col, pages))
         return pd.concat(l)
 
-    def create_df(self) -> pd.DataFrame:
-        
-            data = self.get_all()
-            df = data.explode(self.col)
-            df = df.loc[df[self.col].notnull(), ['id', 'name', self.col]]
-            df[self.col+'_id'] =  df[self.col].str.extract(r'(\d+)').astype(dtype=int, errors='ignore')
+    def df_to_sql(self, df: pd.DataFrame = None, name: str = None, ):
 
-            df.to_sql('locations', con=engine)
-            
+        df = df.to_sql(name=name, con=engine, if_exists='append', index=True, index_label='id')
+        logger.info('Inserted %s rows' %(df))
 
 class Episodes(Client):
     def __init__(self) -> None:
@@ -40,13 +41,35 @@ class Episodes(Client):
         self.url = self.url + 'episode'
         self.col = 'characters'
 
-class Characters(Client):
+    def to_df(self) -> pd.DataFrame:
+        pass
+
+class Residency(Client):
     def __init__(self) -> None:
         super().__init__()
-        self.url = self.url + 'character'
+        self.url = self.url + 'location'
+
+    def to_df(self) -> pd.DataFrame:
+        pass
+
 
 class Locations(Client):
     def __init__(self) -> None:
         super().__init__()
         self.url = self.url + 'location'
         self.col = 'residents'
+
+    def to_df(self) -> pd.DataFrame:
+
+        data = self.get_all()
+        df = data.explode(self.col)
+        df = df.loc[df[self.col].notnull(), ['id', 'name', self.col]]
+        df[self.col+'_id'] =  df[self.col].str.extract(r'(\d+)').astype(dtype=int, errors='ignore')
+
+        df = (df.reset_index()
+              .rename(columns={'id':'location_id'})
+              .drop(['index', 'name', 'residents'], axis=1))
+
+        return df
+
+        
